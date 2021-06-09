0c7f5a8126d18bf97d7a1353538e980f50f006fe
AidenKwong <84517297+Aiden-Kwong@users.noreply.github.com>
2021-06-09 13:55:39 +0000

    linter fix

--- script.js
+++ script.js
@@ -13,45 +13,44 @@ function runComparison () {
   // If the input isn't a number, do this:
   if (isNaN(userAge)) {
     console.log('NaN')
-    
+
     // Display this error message in red.
-    document.getElementById('response').style.color = "#ff0000"
-    document.getElementById('response').innerHTML = "Please enter an age using numerical symbols."
+    document.getElementById('response').style.color = '#ff0000'
+    document.getElementById('response').innerHTML = 'Please enter an age using numerical symbols.'
 
   // If the user age is less than 0 or above 200 do this:
   } else if (userAge <= 0 || userAge > 200) {
     console.log('notrealage')
 
     // Display this error message in red.
-    document.getElementById('response').style.color = "#ff0000"
-    document.getElementById('response').innerHTML = "Please enter a real age."
+    document.getElementById('response').style.color = '#ff0000'
+    document.getElementById('response').innerHTML = 'Please enter a real age.'
 
   // If none of these conditions are true, do this:
   } else {
     // If the age is 17 or up, display this message in green.
     if (userAge >= 17) {
       console.log('r rated movies')
-      document.getElementById('response').style.color = "#274e13"
-      document.getElementById('response').innerHTML = "You can watch movies rated up to R, so you can watch any movie."
+      document.getElementById('response').style.color = '#274e13'
+      document.getElementById('response').innerHTML = 'You can watch movies rated up to R, so you can watch any movie.'
 
     // If the age is 13 or up, display this message in green.
     } else if (userAge >= 13) {
       console.log('pg13')
-      document.getElementById('response').style.color = "#274e13"
-      document.getElementById('response').innerHTML = "You can watch movies rated up to PG-13."
+      document.getElementById('response').style.color = '#274e13'
+      document.getElementById('response').innerHTML = 'You can watch movies rated up to PG-13.'
 
     // If the age is 5 or up, display this message in green.
     } else if (userAge >= 5) {
       console.log('g or pg')
-      document.getElementById('response').style.color = "#274e13"
-      document.getElementById('response').innerHTML = "You can watch movies rated G or PG."
+      document.getElementById('response').style.color = '#274e13'
+      document.getElementById('response').innerHTML = 'You can watch movies rated G or PG.'
 
     // If the age is less than 5, display this message in green.
     } else {
       console.log('too young.')
-      document.getElementById('response').style.color = "#274e13"
-      document.getElementById('response').innerHTML = "You’re too young to watch most movies."
+      document.getElementById('response').style.color = '#274e13'
+      document.getElementById('response').innerHTML = 'You\'re too young to watch most movies.'
     }
-   
   }
 }
